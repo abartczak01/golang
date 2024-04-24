6db8a139edbfa64b35aaf86be6792d2f9c45e6ec
abartczak629 <a.bartczak.629@studms.ug.edu.pl>
2024-04-24 18:51:10 +0200

    added viusalization for burning forest

--- zad02/go.mod
+++ zad02/go.mod
@@ -2,4 +2,9 @@ module example/zad02
 
 go 1.22.0
 
-require github.com/enescakir/emoji v1.0.0 // indirect
+require (
+	github.com/enescakir/emoji v1.0.0 // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.15.0 // indirect
+)
--- zad02/zad02.go
+++ zad02/zad02.go
@@ -3,8 +3,19 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/enescakir/emoji"
+	"github.com/fogleman/gg"
+
+	"image"
+	"image/color/palette"
+	"image/draw"
+	"image/gif"
+	"os"
 )
 
 type Forest struct {
@@ -56,7 +67,7 @@ func (f *Forest) displayForest() {
 	}
 }
 
-func (f *Forest) shootLightning() bool {
+func (f *Forest) shootLightning(toVizualize bool) bool {
 	x := rand.Intn(f.rows)
 	y := rand.Intn(f.columns)
 	if f.forest[x][y] == 0 {
@@ -65,27 +76,40 @@ func (f *Forest) shootLightning() bool {
 	} else {
 		fmt.Printf("Uderzenie pioruna w drzewo (%v, %v)!! Zaczyna się pożar! %v\n", y, x, emoji.AstonishedFace)
 		f.forest[x][y] = 2
-		f.spreadFire(x, y)
+		if toVizualize {
+			f.saveAsImage("forest_pics/forest_1.gif")
+		}
+		f.spreadFire(x, y, toVizualize)
 		return true
 	}
 }
 
 // z modyfikacją z wiatrem trzeba zmienić tylko neighbours
-func (f *Forest) spreadFire(x, y int) {
+func (f *Forest) spreadFire(x, y int, toVizualize bool) {
 	neighbors := [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
 
 	for _, offset := range neighbors {
 		newX, newY := x+offset[0], y+offset[1]
 		if newX >= 0 && newX < f.rows && newY >= 0 && newY < f.columns && f.forest[newX][newY] == 1 {
 			f.forest[newX][newY] = 2
-			// f.DisplayForest()
-			// fmt.Println()
-			f.spreadFire(newX, newY)
+			if toVizualize {
+				var fireNumber = f.countFire()
+				if fireNumber % 5 == 0 {
+					filename := fmt.Sprintf("forest_pics/forest_%d.gif", f.countFire())
+					f.saveAsImage(filename)
+				}
+			}
+			f.spreadFire(newX, newY, toVizualize)
 		}
 	}
 }
 
 func (f *Forest) getStatistics() float32{
+	var burned = f.countFire()
+	return float32(burned)/float32(f.treeNumber)
+}
+
+func (f *Forest) countFire() int {
 	var burned int = 0
 	for i := 0; i < f.rows; i++  {
 		for _, place := range f.forest[i] {
@@ -94,23 +118,129 @@ func (f *Forest) getStatistics() float32{
 			}
 		}
 	}
-	return float32(burned)/float32(f.treeNumber)
+	return burned
+}
+
+func (f *Forest) saveAsImage(filename string) error {
+    const cellSize = 10
+    width := f.columns * cellSize
+    height := f.rows * cellSize
+
+    dc := gg.NewContext(width, height)
+
+    for row := 0; row < f.rows; row++ {
+        for col := 0; col < f.columns; col++ {
+            switch f.forest[row][col] {
+            case 0:
+                dc.SetHexColor("#ffffff") // White color for empty space
+            case 1:
+                dc.SetHexColor("#1f8c1f") // Green color for trees
+            case 2:
+                dc.SetHexColor("#ff0000") // Red color for fire
+            }
+            dc.DrawRectangle(float64(col*cellSize), float64(row*cellSize), float64(cellSize), float64(cellSize))
+            dc.Fill()
+        }
+    }
+
+    img := dc.Image()
+
+    paletted := image.NewPaletted(img.Bounds(), palette.Plan9)
+    draw.FloydSteinberg.Draw(paletted, img.Bounds(), img, image.ZP)
+
+    file, err := os.Create(filename)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    if err := gif.Encode(file, paletted, nil); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func createGIFFromImages() error {
+    folderPath := "forest_pics"
+    files, err := os.ReadDir(folderPath)
+    if err != nil {
+        return err
+    }
+
+    sort.Slice(files, func(i, j int) bool {
+        num1, _ := strconv.Atoi(strings.TrimSuffix(strings.TrimPrefix(files[i].Name(), "forest_"), ".gif"))
+        num2, _ := strconv.Atoi(strings.TrimSuffix(strings.TrimPrefix(files[j].Name(), "forest_"), ".gif"))
+        return num1 < num2
+    })
+
+    outGif := &gif.GIF{}
+    for _, file := range files {
+        f, err := os.Open(filepath.Join(folderPath, file.Name()))
+        if err != nil {
+            return err
+        }
+        defer f.Close()
+        inGif, _, err := image.Decode(f)
+        if err != nil {
+            return err
+        }
+
+        outGif.Image = append(outGif.Image, inGif.(*image.Paletted))
+        outGif.Delay = append(outGif.Delay, 0)
+    }
+
+    f, err := os.OpenFile("out.gif", os.O_WRONLY|os.O_CREATE, 0600)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
+
+    if err := gif.EncodeAll(f, outGif); err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func runSimulationWithVisualization(rows, columns int, percentage float32) {
+	f := newForest(rows, columns, percentage)
+	err := f.saveAsImage("forest_pics/forest_0.gif")
+	if err != nil {
+		fmt.Println("Błąd podczas zapisywania obrazu:", err)
+	}
+	isBurned := f.shootLightning(true)
+	for !isBurned{
+		isBurned = f.shootLightning(true)
+	}
+	f.saveAsImage("forest_pics/forest_10000000000000000000000000000.gif")
+	fmt.Println("Tworzymy gifa!")
+	err1 := createGIFFromImages()
+	if err1 != nil {
+		println("Błąd tworzenia GIF:", err1)
+	}
+
 }
 
 func main() {
-	var rows int = 10
+	var rows int = 40
 	var columns int = 40
 	var percentage float32 = 0.5
+	// var toVizualize bool = true
+	runSimulationWithVisualization(rows, columns, percentage)
 
-	f := newForest(rows, columns, percentage)
-	fmt.Printf("Las o wymiarach %v na %v przed uderzeniem pioruna:\n", rows, columns)
-	f.displayForest()
 
-	fmt.Println("\nWywołujemy uderzenie pioruna...")
-	f.shootLightning()
-	f.displayForest()
-	var stats float32 = f.getStatistics()
-	fmt.Printf("Spalono %.2f%% drzew\n", stats*100)
+	// f := newForest(rows, columns, percentage)
+	// fmt.Printf("Las o wymiarach %v na %v przed uderzeniem pioruna:\n", rows, columns)
+	// f.displayForest()
+
+	// fmt.Println("\nWywołujemy uderzenie pioruna...")
+	// isBurned := f.shootLightning(toVizualize)
+	
+	// f.displayForest()
+	// var stats float32 = f.getStatistics()
+	// fmt.Printf("Spalono %.2f%% drzew\n", stats*100)
+
 
 }
 
@@ -121,7 +251,6 @@ func main() {
 // 2. Dla każdego percentage obliczyć średni poziom spalenia lasu
 // 3. Dla każdego percentage obliczyć stosunek: (zalesienie)/(średni poziom spalenia lasu)
 // 3. Znaleźć największy (najlepszy) stosunek
-// maks zalesienie, min straty
 
 // Test można przeprowadzić na lasach o różnych wymiarach, ale o tej samej powierzchni, np.
-// powierzchnia 400: 20x20, 16x25, 10x40, 8x50
+// powierzchnia 400: 20x20, 16x25, 10x40, 8x50, 5x80, 4x100, 2x200
